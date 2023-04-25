2a77e2c21c7ec4569c842af00477ecfea093ae32
CHRISCHAN-NB\chris <chris.chan@rvautotech.com>
2023-04-25 17:01:09 +0800

    Debug

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/schmidt-arcs-website'),
+      dir: require('path').join(__dirname, './coverage/rv-arcs-website'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
