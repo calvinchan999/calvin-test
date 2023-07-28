ede27d4548f7654349cb4ac0c7cc39e7e7d501e0
calvin <exe985632@gmail.com>
2023-07-28 09:51:54 +0800

    Update: ci workflow

--- replace.build.js
+++ replace.build.js
@@ -1,6 +1,6 @@
 var replace = require('replace-in-file');
 var app = process.argv[2];
-var isArcs = app.toUpperCase() == "ARCS" || app.toUpperCase() == "AZURE" || app.toUpperCase() == "QMH";
+var isArcs = app.toUpperCase() == "ARCS" || app.toUpperCase() == "AZURE" || app.toUpperCase() == "QMH" || app.toUpperCase() == "TLH" || app.toUpperCase() == "MTR";
 // var isAzure = app.toUpperCase() == "AZURE" || app.toUpperCase() == "QMH";
 // const isOther = (app.toUpperCase() === 'ARCS' || app.toUpperCase() === 'AZURE') ? null : 'qmh';
 // app = app.toUpperCase() == "AZURE" ? "arcs" : app
